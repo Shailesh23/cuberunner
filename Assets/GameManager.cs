36b6f59a2b2bdff6b4f890f9ea98e097c22773db
Shailesh Pant <shaileshpant23@gmail.com>
2020-04-17 22:38:42 +0530

    created basic project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartTime = 1f;
+    public GameObject completeLevelUI;
+
+    public void GameOver()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GameOver");
+
+            Invoke("RestartGame", restartTime);
+        }
+        
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void CompleteLevel()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 2) {
+            completeLevelUI.SetActive(true);
+            Debug.Log("last level compelted");
+            Invoke("CloseGame", restartTime);
+        } else {
+            completeLevelUI.SetActive(true);
+            Debug.Log("last level compelted");
+            Invoke("LoadNextScene", restartTime);
+        }
+        
+    }
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rigidBody;
+    public float forwardForce = 500f;
+    public float sidewaysForce = 100f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        rigidBody.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rigidBody.AddForce(sidewaysForce * Time.deltaTime , 0, 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("a"))
+        {
+            rigidBody.AddForce(-sidewaysForce * Time.deltaTime , 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rigidBody.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+}
